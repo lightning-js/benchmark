40b153e7a8487b0e73b4aa8799ae283a76d358ba
Wouter lucas van Boesschoten <wouter@wouterlucas.com>
2024-05-14 20:57:37 +0200

    Add swap, remove, createLots, append and clear

--- renderer/index.js
+++ renderer/index.js
@@ -47,6 +47,7 @@ const createRow = (parent, config = {}) => {
         parent: holder,
         text: text,
         alpha: 0.8,
+        fontFamily: 'Ubuntu-ssdf',
         color: textColor || 0xFFFFFFFF,
         fontSize: 26,
     });
@@ -60,8 +61,8 @@ const createRow = (parent, config = {}) => {
     }
 
     if (lastX >= appWidth) {
-        lastX = Math.random() * 10;
-        lastY = Math.random() * 10;
+        lastX = Math.floor(Math.random() * 10);
+        lastY = Math.floor(Math.random() * 10);
     }
 }
 
@@ -111,6 +112,30 @@ const createMany = (amount = 1000) => {
     });
 }
 
+const appendMany = (amount = 1000) => {
+    return new Promise((resolve) => {
+        const append = () => {
+            for (let i = 0; i < amount; i++) {
+                createRow(rootNode, {
+                    xOffset: 0,
+                    yOffset: 40,
+                    color: pick(colours),
+                    textColor: pick(colours),
+                    text: `${pick(adjectives)} ${pick(nouns)}`
+                });
+            }
+        }
+
+        const appendPerf = performance.now();
+        append();
+
+        renderer.once('idle', () => {
+            const time = performance.now() - appendPerf;
+            resolve({ time });
+        });
+    });
+}
+
 const updateMany = (count, skip = 0) => {
     return new Promise((resolve) => {
         // slice count from parentNode's children
@@ -143,6 +168,39 @@ const updateMany = (count, skip = 0) => {
     });
 }
 
+const swapRows = (count) => {
+    return new Promise((resolve) => {
+        if (rootNode.children.length < 998) {
+            return resolve();
+        }
+
+        const a = rootNode.children[998];
+        const b = rootNode.children[1];
+        if (!a || !b) {
+            return resolve();
+        }
+
+        const swapPerf = performance.now();        
+        const temp = a;
+        a.y = b.y;
+        a.x = b.x;
+        a.color = b.color;
+        a.children[0].color = b.children[0].color;
+        a.children[0].text = b.children[0].text;
+
+        b.y = temp.y;
+        b.x = temp.x;
+        b.color = temp.color;
+        b.children[0].color = temp.children[0].color;
+        b.children[0].text = temp.children[0].text;
+        
+        renderer.once('idle', () => {
+            const time = performance.now() - swapPerf;
+            resolve({ time });
+        });
+    });
+}
+
 const selectRandomNode = () => {
     return new Promise((resolve) => {
         const randomNode = rootNode.children[Math.floor(Math.random() * rootNode.children.length)];
@@ -165,6 +223,24 @@ const selectRandomNode = () => {
 
         renderer.once('idle', () => {
             const time = performance.now() - selectPerf;
+            resolve({ time });
+        });
+    });
+}
+
+const removeRow = () => {
+    return new Promise((resolve) => {
+        // select a random child node
+        const node = rootNode.children[Math.floor(Math.random() * rootNode.children.length)];
+        if (!node) {
+            return resolve();
+        }
+
+        const removePerf = performance.now();
+        node.destroy();
+
+        renderer.once('idle', () => {
+            const time = performance.now() - removePerf;
             resolve({ time });
         });
     });
@@ -206,25 +282,52 @@ results.create = createRes.time.toFixed(2);
 
 await createMany(1000);
 await warmup(updateMany, 1000, 5);
-await clear();
 await createMany(1000);
 const updateManyRes = await updateMany(1000);
 results.update = updateManyRes.time.toFixed(2);
 
 await createMany(1000);
 await warmup(updateMany, [1000, 10], 5);
-await clear();
 await createMany(1000);
 const skipNthRes = await updateMany(1000, 10);
 results.skipNth = skipNthRes.time.toFixed(2);
 
+
 await createMany(1000);
 await warmup(selectRandomNode, undefined, 5);
-await clear();
 await createMany(1000);
 const selectRes = await selectRandomNode();
 results.select = selectRes.time.toFixed(2);
 
+await createMany(1000);
+await warmup(swapRows, undefined, 5);
+await createMany(1000);
+const swapRes = await swapRows();
+results.swap = swapRes.time.toFixed(2);
+
+await createMany(1000);
+await warmup(removeRow, undefined, 5);
+await createMany(1000);
+const removeRes = await removeRow();
+results.remove = removeRes.time.toFixed(2);
+
+await warmup(createMany, 6000, 5);
+const createResLots = await createMany(6000)
+results.createLots = createResLots.time.toFixed(2);
+
+await clear();
+await warmup(appendMany, 1000, 5);
+await createMany(1000);
+const appendRes = await appendMany(1000);
+results.append = appendRes.time.toFixed(2);
+
+await warmup(createMany, 1000, 5);
+const clearRes = await clear();
+results.clear = clearRes.time.toFixed(2);
+
 Object.keys(results).forEach(key => {
     console.log(`${key}: ${results[key]}ms`);
 });
+
+// save it for the results page
+localStorage.setItem('renderer', JSON.stringify(results));
