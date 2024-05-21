db182582eb16e826327cfd5b816f0b59f60b72fb
Michiel van der Geest <hello@michielvandergeest.com>
2024-05-21 14:12:50 +0200

    Blits updates.

--- frameworks/blits/src/App.js
+++ frameworks/blits/src/App.js
@@ -1,19 +1,10 @@
 import Blits from '@lightningjs/blits'
 import { colourNames, adjectives, nouns } from '../../../shared/data.js'
-
-import code from './generated-code/patch6.js'
+import { warmup } from '../../../shared/utils/warmup'
 
 const pick = (dict) => dict[Math.round(Math.random() * 1000) % dict.length]
 
 let perf
-let currentTest
-
-const startPerf = (test) => {
-  currentTest = test
-  perf = performance.now()
-}
-
-const clearPerf = () => (currentTest = false)
 
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 const sequence = (steps, pause = 0) => {
@@ -25,40 +16,93 @@ const sequence = (steps, pause = 0) => {
   }, Promise.resolve(null))
 }
 
+const results = {}
+
+const printResults = () => {
+  Object.entries(results).forEach((item) => {
+    console.log(item[0], item[1].time.toFixed(2) + 'ms')
+  })
+}
+
 let count = 1
 
-const createMany = (amount) => {
-  const items = []
-
-  let x = 0
-  let y = 0
-  for (let i = 1; i < amount + 1; i++) {
-    items.push({
-      id: 'key' + count++,
-      x: x,
-      y: y,
-      color: pick(colourNames),
-      textColor: pick(colourNames),
-      text: `${pick(adjectives)} ${pick(nouns)}`,
-    })
-
-    x = (i % 27) * 40
-    y = ~~(i / 27) * 40
+const createItem = (x, y, key) => {
+  return {
+    id: key || 'key' + count++,
+    x: x,
+    y: y,
+    w: 200,
+    h: 40,
+    color: pick(colourNames),
+    textColor: pick(colourNames),
+    text: `${pick(adjectives)} ${pick(nouns)}`,
   }
+}
 
-  return items
+let done
+
+const clear = function () {
+  return new Promise((resolve) => {
+    done = resolve
+    this.items = []
+  })
+}
+
+const createMany = function (amount) {
+  return new Promise((resolve) => {
+    clear.call(this).then(() => {
+      done = resolve
+      const items = []
+
+      let x = 0
+      let y = 0
+      for (let i = 1; i < amount + 1; i++) {
+        items.push(createItem(x, y))
+
+        x = (i % 27) * 40
+        y = ~~(i / 27) * 40
+      }
+
+      this.items = items
+    })
+  })
+}
+
+const updateMany = function (skip = 0) {
+  return new Promise((resolve) => {
+    done = resolve
+    for (let i = 0; i < this.items.length; i += skip + 1) {
+      this.items[i].color = pick(colourNames)
+      this.items[i].textColor = pick(colourNames)
+      this.items[i].text = `${pick(adjectives)} ${pick(nouns)}`
+    }
+  })
+}
+
+const updateRandom = function () {
+  return new Promise((resolve) => {
+    done = resolve
+
+    const randomItem = this.items[Math.floor(Math.random() * this.items.length)]
+
+    randomItem.x = 1200
+    randomItem.y = 100
+    randomItem.color = 'red'
+    randomItem.w = 1200
+    randomItem.h = 400
+  })
 }
 
 export default Blits.Application({
   template: `
     <Element>
-      <Element :for="item in $items" w="200" h="40" color="$item.color" x="$item.x" y="$item.y" key="$item.id">
-        <Text content="$item.text" color="$item.textColor" />
+      <Element :for="item in $items" w="200" h="40" :color="$item.color" x="$item.x" y="$item.y" key="$item.id">
+        <Text content="$item.text" :color="$item.textColor" alpha="0.8" size="26" font="Ubuntu" x="5" y="2" />
       </Element>
     </Element>
   `,
 
-  _code: code,
+  // code: code,
 
   state() {
     return {
@@ -66,59 +110,55 @@ export default Blits.Application({
     }
   },
   hooks: {
-    ready() {
-      sequence([() => this.test1()], 3000)
+    async ready() {
+      sequence([
+        () => this.testCreateMany(),
+        () => this.testUpdateMany(),
+        // () => this.testSkipNth(),
+        // () => this.testUpdateRandom(),
+        // () => this.testCreateMuchoMany(),
+        () => printResults(),
+      ])
     },
     idle() {
-      if (currentTest) {
+      if (done) {
         const now = performance.now()
-        console.log(currentTest, now - perf)
+        const time = now - perf
         perf = now
+        done({ time })
+        done = null
       }
     },
   },
   methods: {
-    test1() {
-      sequence(
-        [
-          () => {
-            this.items = createMany(1000)
-          },
-          () => {
-            this.items = []
-          },
-          () => {
-            this.items = createMany(1000)
-          },
-          () => {
-            this.items = []
-          },
-          () => {
-            this.items = createMany(1000)
-          },
-          () => {
-            this.items = []
-          },
-          () => {
-            this.items = createMany(1000)
-          },
-          () => {
-            this.items = []
-          },
-          () => {
-            this.items = createMany(1000)
-          },
-          () => {
-            this.items = []
-          },
-          () => {
-            startPerf('Create 1000')
-            const items = createMany(1000)
-            this.items = items
-          },
-        ],
-        200
-      )
+    empty() {
+      this.items = []
+    },
+    async testCreateMany() {
+      await warmup(createMany.bind(this), 1000, 5)
+      results.create = await createMany.call(this, 1000)
+    },
+    async testUpdateMany() {
+      await createMany.call(this, 1000)
+      await warmup(updateMany.bind(this), 0, 5)
+      await createMany.call(this, 1000)
+      results.update = await updateMany.call(this, 0)
+    },
+    async testSkipNth() {
+      await createMany.call(this, 1000)
+      await warmup(updateMany.bind(this), 10, 5)
+      await createMany.call(this, 1000)
+      results.skipNth = await updateMany.call(this, 10)
+    },
+    async testUpdateRandom() {
+      await createMany.call(this, 1000)
+      await warmup(updateRandom.bind(this), null, 5)
+      await createMany.call(this, 1000)
+      results.select = await updateRandom.call(this)
+    },
+    async testCreateMuchoMany() {
+      await warmup(createMany.bind(this), 10000, 5)
+      results.createLots = await createMany.call(this, 10000)
     },
   },
 })
