5af575824c5f5fa5f01d041d85904fcd80649bd8
wouterlucas <wouter@wouterlucas.com>
2024-07-21 14:39:27 +0200

    Add some jsdoc annotations

---
+++ testrunner/types/memoryResults.js
@@ -0,0 +1,10 @@
+/**
+ * @typedef {Object} MemoryResult
+ * @property {number} heap - The memory heap size.
+ * @property {number} create - The memory creation time.
+ * 
+ * @typedef {Object} MemoryResults
+ * @property {string} framework - The framework name.
+ * @property {MemoryResult} results - The memory benchmark results.
+ */
+export {}
