3938d9cc8546360db25b15b9b15102a48c8cf904
wouterlucas <wouter@wouterlucas.com>
2024-10-07 11:56:20 +0200

    Refactor react code for consistency and robustness

--- frameworks/react/src/pages/Benchmark.jsx
+++ frameworks/react/src/pages/Benchmark.jsx
@@ -1,9 +1,11 @@
 import { Component } from 'react';
 
-import { colours, adjectives, nouns } from '../../../../shared/data';
-import { warmup } from "../../../../shared/utils/warmup";
+import { colours, adjectives, nouns } from '../../../../shared/data.js';
+import { warmup } from "../../../../shared/utils/warmup.js";
+import { waitUntilIdle } from '../../../../shared/utils/waitUntilIdle.js';
+import { run } from '../../../../shared/utils/run.js';
 
-import { pick } from "../../../../shared/utils/pick";
+import { pick } from "../../../../shared/utils/pick.js";
 import { getRenderer } from "../utils/renderer";
 
 function buildData(count) {
@@ -32,6 +34,8 @@ export class Benchmark extends Component {
             data: []
         }
 
+        this.renderer = getRenderer();
+
         setTimeout(() => {
             this.runBenchmark();
         }, 1000);
@@ -41,18 +45,18 @@ export class Benchmark extends Component {
         return this.clear().then(() => {
             return new Promise((resolve) => {
                 const createPerf = performance.now();
-                getRenderer().once('idle', () => {
-                    resolve({ time: performance.now() - createPerf});
+
+                waitUntilIdle(this.renderer, createPerf).then( (time) => {
+                    resolve({ time });
                 });
 
-                // setData(buildData(amount))
                 this.setState({ data: buildData(amount) });
             });
         });
   }
 
-  clear = () => { 
-      return new Promise((resolve) => {
+    clear = () => { 
+        return new Promise((resolve) => {
             const data = this.state.data;
             if (data.length === 0) {
                 resolve({ time: 0 });
@@ -60,26 +64,40 @@ export class Benchmark extends Component {
             }
 
             const clearPerf = performance.now();
-            getRenderer().once('idle', () => {
-                resolve({ time: performance.now() - clearPerf });
+            waitUntilIdle(this.renderer, clearPerf).then( (time) => {
+                resolve({ time });
             });
             
             this.setState({ data : [] });
-      });
-  }
+        });
+    }
+
+    clearTest = () => {
+        return new Promise( resolve => {  
+            waitUntilIdle(this.renderer, performance.now()).then( time => {
+                this.clear().then( time => {
+                    resolve(time);
+                });
+            });
+   
+            this.setState({ data: buildData(1000) });
+        });
+    }
   
-  appendMany = (amount = 1000) => {
+    appendMany = (amount = 1000) => {
       return new Promise((resolve) => {
-          const appendPerf = performance.now();
-          getRenderer().once('idle', () => {
-              resolve({ time: performance.now() - appendPerf });
-          });
+            this.createMany(1000).then( () => {
+                const appendPerf = performance.now();
+                waitUntilIdle(this.renderer, appendPerf).then( (time) => {
+                    resolve({ time });
+                });
 
-          this.setState({ data: this.state.data.concat(buildData(amount)) });
-      });
-  }
+                this.setState({ data: this.state.data.concat(buildData(amount)) });
+            });
+        });
+    }
 
-  updateMany = (count = 1000, skip = 0) => {
+    updateMany = (count = 1000, skip = 0) => {
         return new Promise((resolve) => {
             const updatePerf = performance.now();
 
@@ -91,17 +109,20 @@ export class Benchmark extends Component {
                 node.textColor = pick(colours);
             }
 
-            getRenderer().once('idle', () => {
-                resolve({ time: performance.now() - updatePerf });
+            waitUntilIdle(this.renderer, updatePerf).then( (time) => {
+                resolve({ time });
             });
 
             this.setState({ data });
-      });
-  }
+        });
+    }
   
-  swapRows = () => {
-      return new Promise((resolve) => {
+    swapRows = () => {
+        return new Promise((resolve) => {
             const swapPerf = performance.now();
+            waitUntilIdle(this.renderer, swapPerf).then( (time) => {
+                resolve({ time });
+            });
             
             const newdata = [...this.state.data];
             const d1 = newdata[1];
@@ -110,19 +131,15 @@ export class Benchmark extends Component {
             newdata[1] = d998;
             newdata[998] = d1;
 
-            getRenderer().once('idle', () => {
-                resolve({ time: performance.now() - swapPerf });
-            });
-
             this.setState({ data: newdata });
-      });
-  }
+        });
+    }
 
     selectRandom = () => {
         return new Promise((resolve) => {
             const selectPerf = performance.now();
-            getRenderer().once('idle', () => {
-                resolve({ time: performance.now() - selectPerf });
+            waitUntilIdle(this.renderer, selectPerf).then( (time) => {
+                resolve({ time });
             });
 
             const selected = this.state.data[Math.floor(Math.random() * this.state.data.length)];
@@ -134,75 +151,73 @@ export class Benchmark extends Component {
             selected.fontSize = 100;
 
             this.setState({ data: this.state.data });
-      });
-  }
+        });
+    }
 
-  removeRow = () => {
-      return new Promise((resolve) => {
-          const removePerf = performance.now();
-          getRenderer().once('idle', () => {
-              resolve({ time: performance.now() - removePerf });
-          });
+    removeRow = () => {
+        return new Promise((resolve) => {
+            const removePerf = performance.now();
+            waitUntilIdle(this.renderer, removePerf).then( (time) => {
+                resolve({ time });
+            });
 
-          this.state.data.pop();
-          this.setState({ data: this.state.data });
-      });
-  }
+            this.state.data.pop();
+            this.setState({ data: this.state.data });
+        });
+    }
 
     runBenchmark = async () => {
         console.log('running benchmark');
 
-        const { createMany, clear, updateMany, appendMany, swapRows, selectRandom, removeRow } = this;
+        const { createMany, clear, updateMany, appendMany, swapRows, selectRandom, removeRow, clearTest } = this;
 
         const results = {};
 
-        await warmup(createMany, 1000, 5)
-        const createRes = await createMany(1000)
-        results.create = createRes.time.toFixed(2);
+        await warmup(createMany, 1000, 5);
+        const { average: createAvg, spread: createSpread } = await run(createMany, 1000, 5);
+        results.create = `${createAvg.toFixed(2)}ms ±${createSpread.toFixed(2)}`;
 
         await createMany(1000);
         await warmup(updateMany, 1000, 5);
         await createMany(1000);
-        const updateRes = await updateMany(1000, 0);
-        results.update = updateRes.time.toFixed(2);
-
+        const { average: updateAvg, spread: updateSpread } = await run(updateMany, 1000, 5);
+        results.update = `${updateAvg.toFixed(2)}ms ±${updateSpread.toFixed(2)}`;
+    
         await createMany(1000);
         await warmup(updateMany, [1000, 10], 5);
         await createMany(1000);
-        const skipNthRes = await updateMany(1000, 10);
-        results.skipNth = skipNthRes.time.toFixed(2);
-
+        const { average: skipNthAvg, spread: skipNthSpread } = await run(updateMany, [1000, 10], 5);
+        results.skipNth = `${skipNthAvg.toFixed(2)}ms ±${skipNthSpread.toFixed(2)}`;
+    
         await createMany(1000);
         await warmup(selectRandom, undefined, 5);
         await createMany(1000);
-        const selectRes = await selectRandom();
-        results.select = selectRes.time.toFixed(2);
-
+        const { average: selectAvg, spread: selectSpread } = await run(selectRandom, undefined, 5);
+        results.select = `${selectAvg.toFixed(2)}ms ±${selectSpread.toFixed(2)}`;
+    
         await createMany(1000);
         await warmup(swapRows, undefined, 5);
         await createMany(1000);
-        const swapRes = await swapRows();
-        results.swap = swapRes.time.toFixed(2);
-
+        const { average: swapAvg, spread: swapSpread } = await run(swapRows, undefined, 5);
+        results.swap = `${swapAvg.toFixed(2)}ms ±${swapSpread.toFixed(2)}`;
+    
         await createMany(1000);
         await warmup(removeRow, undefined, 5);
         await createMany(1000);
-        const removeRes = await removeRow();
-        results.remove = removeRes.time.toFixed(2);
-
+        const { average: removeAvg, spread: removeSpread } = await run(removeRow, undefined, 5);
+        results.remove = `${removeAvg.toFixed(2)}ms ±${removeSpread.toFixed(2)}`;
+    
         await warmup(createMany, 10000, 5);
-        const createResLots = await createMany(10000)
-        results.createLots = createResLots.time.toFixed(2);
-
-        await clear();
+        const { average: createLotsAvg, spread: createLotsSpread } = await run(createMany, 10000, 5);
+        results.createLots = `${createLotsAvg.toFixed(2)}ms ±${createLotsSpread.toFixed(2)}`;
+    
         await warmup(appendMany, 1000, 5);
-        await createMany(1000);
-        const appendRes = await appendMany(1000);
-        results.append = appendRes.time.toFixed(2);
-
-        await warmup(createMany, 1000, 5);
-        const clearRes = await clear();
-        results.clear = clearRes.time.toFixed(2);
+        const { average: appendAvg, spread: appendSpread } = await run(appendMany, 10000, 5);
+        results.append = `${appendAvg.toFixed(2)}ms ±${appendSpread.toFixed(2)}`;
+    
+        await warmup(clearTest, 1000, 5);
+        const { average: clearAvg, spread: clearSpread } = await run(clearTest, 10000, 5);
+        results.clear = `${clearAvg.toFixed(2)}ms ±${clearSpread.toFixed(2)}`;
 
         Object.keys(results).forEach(key => {
             console.log(`${key}: ${results[key]}ms`);
